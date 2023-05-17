6e6cf4cf9854586fd5024f64937fcd78165dba73
Vedant <vedantnp200@gmail.com>
2023-05-17 23:11:35 +0530

    Program to compare length of Line 1 is greater, smaller or Equal than Line 2

--- Program.cs
+++ Program.cs
@@ -64,11 +64,57 @@ namespace LineComparison
 
         }
 
+        static void CompareTwoLines()
+        {
+            int x11, x12, y11, y12;
+            int x21, x22, y21, y22;
+            double lengthOfLineOne;
+            double lengthOfLineTwo;
 
+            Console.WriteLine("First Line Co-ordinates");
+            Console.WriteLine("Enter x1 value");
+            x11 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter x2 value");
+            x12 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter y1 value");
+            y11 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter y2 value");
+            y12 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Second Line Co-ordinates");
+            Console.WriteLine("Enter x1 value");
+            x21 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter x2 value");
+            x22 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter y1 value");
+            y21 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter y2 value");
+            y22 = Convert.ToInt32(Console.ReadLine());
+
+            lengthOfLineOne = Math.Sqrt(Math.Pow((x12 - x11), 2) + Math.Pow((y12 - y11), 2));
+            lengthOfLineTwo = Math.Sqrt(Math.Pow((x22 - x21), 2) + Math.Pow((y22 - y21), 2));
+
+            if (lengthOfLineOne > lengthOfLineTwo)
+            {
+                Console.WriteLine("Lines 1 is greater than Line 2");
+            }
+            else if (lengthOfLineOne < lengthOfLineTwo) 
+            {
+                Console.WriteLine("Lines 1 is smaller than Line 2");
+            }
+            else
+            {
+                Console.WriteLine("Both lines are equal.");
+            }
+        }
         static void Main(string[] args)
-        { 
+        {
             //Program.LengthOfLine();
-            Program.TwoLinesEqualityCheck();
+
+            //Program.TwoLinesEqualityCheck();
+
+            Program.CompareTwoLines();
+
         }
     }
 }
